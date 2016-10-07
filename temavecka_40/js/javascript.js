35da9a2bb47327a5a1815d91273ee2a92a80f261
ams103 <ams_103@hotmail.com>
2016-10-07 10:00:32 +0200

    Pusha ra!

---
+++ temavecka_40/js/javascript.js
@@ -0,0 +1,19 @@
+
+
+$('.carousel').carousel({
+	interval:6000
+})
+
+
+
+
+/*
+var numItems = $('div.item').length;
+	console.log(numItems);
+
+var i = setInterval(function(){
+	  var currentItem = Math.floor(Math.random() * numItems);
+  console.log(currentItem);
+  $('#myCarousel').carousel(currentItem);
+}, 3000);
+*/
